98969c88574581c7d1086bcf0058d05ff1ce512f
Nya Garcia <nyablk97@gmail.com>
2020-03-29 20:17:15 +0200

    feat(index): inject stringFormatService

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ import { StreamService } from './services/stream.service';
 import { UpdateService } from './services/update.service';
 import { WindowsFilesService } from './services/windows-files.service';
 import { ResultsService } from './services/results.service';
+import { StringFormatService } from './services/string-format.service';
 
 const isOSWindows = () => process.platform === PLATFORMS.WINDOWS;
 
@@ -20,10 +21,13 @@ const fileService: IFileService = isOSWindows()
   ? new WindowsFilesService(streamService)
   : new LinuxFilesService(streamService);
 
+const stringFormatService = new StringFormatService();
+
 export const controller = new Controller(
   fileService,
   new SpinnerService(),
-  new ConsoleService(),
+  stringFormatService,
+  new ConsoleService(stringFormatService),
   new UpdateService(new HttpsService()),
   new ResultsService(),
 );
